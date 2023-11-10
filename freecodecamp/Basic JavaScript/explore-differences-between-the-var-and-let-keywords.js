2b6188f9bf9aaa760ed2f08899e06854ca3e12a9
erelse <erelse@yandex.ru>
2023-11-10 04:34:16 +0300

    9th task

---
+++ freecodecamp/Basic JavaScript/explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+
+// Update the code so it only uses the let keyword:
+// var catName = "Oliver";
+// var catSound = "Meow!";
+
+
+// SOLUTION:
+
+let catName = "Oliver";
+let catSound = "Meow!";
