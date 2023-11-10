6c07c64bdc7052bc0ca20ddceadb3ffbd09fd302
erelse <erelse@yandex.ru>
2023-11-10 04:14:08 +0300

    7th task

---
+++ freecodecamp/Basic JavaScript/understanding-uninitialized-variables.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+// a should be defined and have a final value of 6.
+// Passed:b should be defined and have a final value of 15.
+// Passed:c should not contain undefined and should have a final value of the string I am a String!
+// Passed:You should not change code below the specified comment.
+// Only change code below this line
+
+
+
+//SOLUTION:
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
